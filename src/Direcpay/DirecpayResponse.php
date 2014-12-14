cf7c2d7c5bbf8843911b35231e215e8657e02c99
Rahul makhija <rahulmkhj@gmail.com>
2014-12-14 14:12:26 +0530

    added DirecpayResponse class

---
+++ src/Direcpay/DirecpayResponse.php
@@ -0,0 +1,45 @@
+<?php
+namespace RAHULMKHJ\PaymentGateways\Direcpay;
+
+class DirecpayResponse {
+
+	private $response = NULL;
+	private $details = NULL;
+
+	public function __construct( Array $response_params )
+	{
+		$arr = explode('|', $response_params);
+		$values = array_filter($arr);
+		$keys = ['direcpayreferenceid','flag','country','currency','otherdetails','merchantordernumber','amount'];
+		$result = array_combine($keys, $values);
+		parse_str(urldecode($result['otherdetails']), $this->details);
+		unset($result['otherdetails']);
+		$this->response = $result;
+	}
+
+	public function txnStatus()
+	{
+		return $this->response['status'];
+	}
+
+	public function txnSucceed()
+	{
+		return $this->response['status'] == 'SUCCESS' ? TRUE : FALSE;
+	}
+
+	public function txtFailed()
+	{
+		return $this->response['status'] == 'FAIL' ? TRUE : FALSE;
+	}
+
+	public function getResponse()
+	{
+		return $this->response;
+	}
+
+	public function getDetails()
+	{
+		return $this->details;
+	}
+}
+//end of file DirecpayResponse.php
